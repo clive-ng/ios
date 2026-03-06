c09bc22fda802ec5f64ccf536e4f8f4f48dd9904
clive-ng <128707500+clive-ng@users.noreply.github.com>
2026-03-06 21:23:13 +0000

    Create main.swift

---
+++ main.swift
@@ -0,0 +1,17 @@
+//
+//  M1App.swift
+//  M1
+//
+//  Created by Clive on 05/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct M1App: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashScreenView()
+        }
+    }
+}
